47a3e96b514b9386a9d797522b7ad39bfd6fc66b
Isaac <isaaccheng9@gmail.com>
2020-03-09 13:28:30 +0000

    Added commenting

--- CheckID.java
+++ CheckID.java
@@ -1,3 +1,7 @@
+/*
+Exception handler for the ID of a new bean bag to see if it has a valid format.
+*/
+
 package beanbags;
 
 public class CheckID {
@@ -6,17 +10,19 @@ public class CheckID {
             try {
                 // long decimal = Long.parseLong(id, 16);
                 // long decimal = Long.valueOf(id, 16);
+                // Checks for a negative number (possible due to two's complement
+                // representation).
                 int decimal = Long.valueOf(id, 16).intValue();
                 if (decimal < 0) {
                     throw new IllegalIDException("Invalid Hexadecimal Identifier - Not a positive number");
                 }
-                System.out.println(decimal);
+                // Checks for non-hexadecimal numbers.
             } catch (NumberFormatException e) {
                 throw new IllegalIDException("Invalid Hexadecimal Identifier - Not a hexadecimal number");
             }
+            // Checks for IDs which don't have a length of 8.
         } else {
             throw new IllegalIDException("Invalid Hexadecimal Identifier - Hex is not eight characters in length");
         }
     }
 }
-
