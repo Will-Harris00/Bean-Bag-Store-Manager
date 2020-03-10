a870facf9d173d82ea2e66f031f9aa4abfdeb1da
Isaac <isaaccheng9@gmail.com>
2020-03-10 13:20:15 +0000

    Added comments

--- Reservation.java
+++ Reservation.java
@@ -1,18 +1,19 @@
+// A class to manage the list of reservations.
 package beanbags;
 
 public class Reservation {
     private BeanBag item;
-    // private variable initialized
+    // Initialising private variable for reservation number.
     private int reference;
 
-    // Constructor: initialise identifier, reservation status, original price, customer reference, and customer name
+    // Constructor: initialise reservation list.
     public Reservation(BeanBag item, int size) {
         this.item = item;
-        // elected to use negative hex values as reservation numbers to make them easily distinguishable from beanbag id
-        this.reference = size+1;
+        // Uses negative hex values as reservation numbers to make them easily
+        // distinguishable from bean bag ID.
+        this.reference = size + 1;
     }
 
-    // public getters and setters for private instance
     // Public getters for private instances.
 
     public BeanBag getAttributes() {
@@ -22,7 +23,6 @@ public class Reservation {
     public int getReference() {
         return reference;
     }
-
 
     // Public setters for private instances.
     public void setReference(int reference) {
