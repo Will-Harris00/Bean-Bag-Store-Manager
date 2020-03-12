a042c05ba5a1e2a25f5e0bbe5928ac831ce09e51
Isaac <isaaccheng9@gmail.com>
2020-03-12 23:59:23 +0000

    Fixed formatting and added comments

--- Checks.java
+++ Checks.java
@@ -13,6 +13,7 @@ public class Checks {
         Reservation held;
         int i;
         ObjectArrayList obj;
+
         // Iterates over the lists one by one.
         for (i = 0; i < objects.length; i++) {
             // Accesses each element of array.
@@ -20,13 +21,16 @@ public class Checks {
 
             // Updates the IDs in the available stock, reserved and sold list.
             for (int j = 0; j < obj.size(); j++) {
-
+                // Casts reserved bean bags separately to available/sold bean bags, as they have
+                // different attributes.
                 if (obj == reserved) {
                     held = (Reservation) obj.get(j);
                     item = held.getAttributes();
                 } else {
                     item = (BeanBag) obj.get(j);
                 }
+                // Checks name, manufacturer, and additional information for bean bags with the
+                // same ID.
                 if (newBeanBag.getIdentifier().equalsIgnoreCase(item.getIdentifier())) {
                     // Checks whether names of the bean bags match.
                     if (!newBeanBag.getName().equalsIgnoreCase(item.getName())) {
@@ -53,20 +57,21 @@ public class Checks {
     // Exception handler for the ID of a new bean bag to see if it has a valid
     // format.
     public static void validId(String id) throws IllegalIDException {
+        // Checks whether the ID consists of eight characters.
         if (id.length() == 8) {
             try {
+                // Throws an exception for bean bag IDs which aren't positive hexadecimals.
                 if ((int) Long.parseLong(id, 16) < 0) {
-                    // Throws an exception for bean bag IDs which aren't positive hexadecimals.
                     throw new IllegalIDException(
                             "Invalid hexadecimal identifier, '" + id + "'  is not a positive number");
                 }
-            } catch (NumberFormatException e) {
                 // Throws an exception for bean bag IDs which aren't hexadecimal numbers.
+            } catch (NumberFormatException e) {
                 throw new IllegalIDException(
                         "Invalid hexadecimal identifier, '" + id + "'  is not a hexadecimal number");
             }
-        } else {
             // Throws an exception for IDs which don't have a length of 8.
+        } else {
             throw new IllegalIDException(
                     "Invalid hexadecimal identifier, '" + id + "'  is not eight characters in length");
         }
@@ -75,12 +80,13 @@ public class Checks {
     // Exception handler for reservations.
     public static void validReservation(int num, String id, int available) throws BeanBagNotInStockException,
             InsufficientStockException, IllegalNumberOfBeanBagsReservedException, IllegalIDException {
+        Checks.validId(id);
+
         // Throws an exception if the user attempts to reserve a non-positive number of
         // bean bags.
         if (num <= 0)
             throw new IllegalNumberOfBeanBagsReservedException(
                     "The number of bean bags '" + num + "' reserved cannot be less than zero");
-        Checks.validId(id);
         // Throws an exception if there are no bean bags available for reservation.
         if (available == 0)
             throw new BeanBagNotInStockException("None of these bean bags '" + id + "' are available for reservation");
@@ -94,13 +100,13 @@ public class Checks {
     // Exception handler for sales.
     public static void validSale(int num, String id, int available) throws BeanBagNotInStockException,
             InsufficientStockException, IllegalNumberOfBeanBagsSoldException, IllegalIDException {
+        Checks.validId(id);
+
         // Throws an exception if the user attempts to sell a non-positive number of
         // bean bags.
         if (num <= 0)
             throw new IllegalNumberOfBeanBagsSoldException(
                     "The number of bean bags '" + num + "' sold cannot be less than zero");
-        // Throws an exception if the ID is not in the correct format.
-        Checks.validId(id);
         // Throws an exception if there are no bean bags available for sale.
         if (available == 0)
             throw new BeanBagNotInStockException("None of these bean bags '" + id + "' are available for sale");
