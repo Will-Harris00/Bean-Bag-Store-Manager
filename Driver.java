263749cd3987fcf06ed336b08026392ca1d79f26
Isaac <isaaccheng9@gmail.com>
2020-03-12 17:11:18 +0000

    Tested all methods

--- Driver.java
+++ Driver.java
@@ -29,6 +29,70 @@ public class Driver {
         // System.out.println(driver.getBeanBagDetails("0000bbbb"));
 
         // Getting number of unique bean bags.
-        System.out.println(driver.getNumberOfDifferentBeanBagsInStock());
+        // System.out.println(driver.getNumberOfDifferentBeanBagsInStock());
+
+        // Getting number of total sales.
+        // driver.setBeanBagPrice("0000cccc", 5);
+        // driver.sellBeanBags(3, "0000cccc");
+        // driver.setBeanBagPrice("0000bbbb", 3);
+        // driver.sellBeanBags(2, "0000bbbb");
+        // System.out.println(driver.getNumberOfSoldBeanBags());
+        // System.out.println(driver.getNumberOfSoldBeanBags("0000cccc"));
+
+        // Getting total price of reserved bean bags.
+        // driver.setBeanBagPrice("0000bbbb", 5);
+        // driver.reserveBeanBags(3, "0000bbbb");
+        // driver.setBeanBagPrice("0000cccc", 3);
+        // driver.reserveBeanBags(2, "0000cccc");
+        // System.out.println(driver.getTotalPriceOfReservedBeanBags());
+
+        // Getting total price of sold bean bags.
+        driver.setBeanBagPrice("0000cccc", 5);
+        driver.sellBeanBags(1, "0000cccc");
+        driver.setBeanBagPrice("0000bbbb", 3);
+        driver.sellBeanBags(2, "0000bbbb");
+        System.out.println(driver.getTotalPriceOfSoldBeanBags("0000cccc"));
+
+        // Saving store contents.
+        // driver.saveStoreContents("filename");
+        // driver.array("a");
+        // driver.empty();
+        // driver.array("a");
+        // driver.loadStoreContents("filename");
+        // driver.array("a");
+
+        // Replacing an ID.
+        // driver.array("a");
+        // driver.replace("0000bbbb", "0000dddd");
+        // driver.array("a");
+
+        // Reserving bean bags.
+        driver.addBeanBags(10, "Manufacturer2", "Name3", "0000bcbc", (short) 2002, (byte) 2);
+        System.out.println(driver.reserveBeanBags(3, "0000cccc")); 
+        System.out.println(driver.reserveBeanBags(4, "0000bbbb")); 
+        
+        // Getting number of reserved bean bags in stock.
+        // System.out.println(driver.reservedBeanBagsInStock());
+
+        // Reset sale and cost tracking.
+        // driver.resetSaleAndCostTracking();
+        // driver.array("s");
+
+        // Selling bean bags from reservations.
+        // driver.sellBeanBags(2);
+        // driver.array("s");
+
+        // Setting the price of a bean bag.
+        driver.setBeanBagPrice("0000cccc", 4);
+        driver.array("a");
+        System.out.println(driver.reserveBeanBags(1, "0000cccc")); 
+        driver.setBeanBagPrice("0000cccc", 2);
+        driver.array("a");
+        driver.array("r");
+
+        // Unreserve bean bags.
+        driver.unreserveBeanBags(2);
+        driver.array("a");
+        driver.array("r");
     }
 }
