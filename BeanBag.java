0d1d279eab1c984fad772b1eaa35cbee307ae2fa
Isaac <isaaccheng9@gmail.com>
2020-03-13 00:25:08 +0000

    Ordered methods alphabetically

--- BeanBag.java
+++ BeanBag.java
@@ -27,60 +27,59 @@ public class BeanBag implements Serializable {
     }
 
     // Public getters for private instances.
-    public String getManufacturer() {
-        return manufacturer;
-    }
-
-    public String getName() {
-        return name;
-    }
-
     public String getIdentifier() {
         return id;
     }
-
-    public short getYear() {
-        return year;
-    }
-
-    public byte getMonth() {
-        return month;
+    
+    public String getManufacturer() {
+        return manufacturer;
     }
 
     public String getInformation() {
         return information;
     }
 
+    public byte getMonth() {
+        return month;
+    }
+
+    public String getName() {
+        return name;
+    }
+
     public int getPriceInPence() {
         return priceInPence;
     }
 
+    public short getYear() {
+        return year;
+    }
     // Public setters for private instances.
-    public void setManufacturer(String manufacturer) {
-        this.manufacturer = manufacturer;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public void setIdentifier(String id) {
         this.id = id;
     }
+    
+    public void setInformation(String information) {
+        this.information = information;
+    }
 
-    public void setYear(short year) {
-        this.year = year;
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
     }
 
     public void setMonth(byte month) {
         this.month = month;
     }
 
-    public void setInformation(String information) {
-        this.information = information;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public void setPriceInPence(int priceInPence) {
         this.priceInPence = priceInPence;
+    }
+
+    public void setYear(short year) {
+        this.year = year;
     }
 }
